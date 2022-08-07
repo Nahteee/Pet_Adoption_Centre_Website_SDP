d8dd1e6a9b8cfa95fa6ad26f1d42621172106428
BlueShark8J <estherlesliebala@gmail.com>
2022-08-07 21:24:43 +0800

    added php files

---
+++ centreform.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+    <form>
+        Centre name:
+        <input type="text">
+        <br>
+        Address:
+        <input type="text">
+        <br>
+        Phone number:
+        <input type="tel">
+        <br>
+        Your email:
+        <input type="email">
+        <br>
+        Centre description:
+        <input type="text">
+        <br>
+        <input type="submit" value="Submit Application">
+    </form>
+</body>
+</html>
