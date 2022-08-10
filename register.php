6766427ed1e7d3647538fedf2b5d41afc6eadb33
SuperTsumu <51882569+supertsumu@users.noreply.github.com>
2022-08-10 23:51:22 +0800

    fixed registration bug
    registration should work now. could use some input sanitation.

--- register.php
+++ register.php
@@ -4,36 +4,27 @@ include("connection.php");
 $sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
-$username = $_POST[username];
-$password = $_POST[psw];
-$firstname = $_POST[firstname];
-$lastname = $_POST[lastname];
-$IC = $_POST[IC];
-$phone = $_POST[new_phone];
-$email = $_POST[email];
-$address = $_POST[address];
-$income = $_POST[income];
-$role = "member";
-
 $valid = 1;
+$rows = mysqli_num_rows ($result);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        if ($row["username"] == $username){
+        if ($row["username"] == $_POST['user_name']){
             $valid = 0;
         }
     }
 }
 
+
 if ($valid == 0){
-    echo '<script type="text/JavaScript"> alert("This username has been taken. Please pick another username"); window.location.href = "/sdp/register.html"; </script>';
+    echo '<script type="text/JavaScript"> alert("This username has been taken. Please pick another username"); window.location.href = "register.html"; </script>';
 } else {
-    $sql="INSERT INTO users (ID, username, 'password', 'role', IC, email, first_name, last_name, phone, income, 'address')
+    $sql="INSERT INTO users (ID, username, password, role, IC, email, first_name, last_name, phone, income, address)
     VALUES
-    (3, '$username','$password', '$role','$IC',
-    '$email' '$firstname','$lastname','$phone', '$income', '$address')"; 
-    //echo '<script type="text/JavaScript"> window.location.href = "/sdp/login.html"; </script>';
+    ('$rows', '$_POST[user_name]','$_POST[psw]', 'member','$_POST[IC]',
+    '$_POST[email]', '$_POST[firstname]','$_POST[lastname]','$_POST[new_phone]', '$_POST[income]', '$_POST[address]')"; 
+    echo '<script type="text/JavaScript"> alert("Account registration successful!"); window.location.href = "login.html"; </script>';
     if (!mysqli_query($conn,$sql)) {
         die('Error: ' . mysqli_error($conn)); 
     };
