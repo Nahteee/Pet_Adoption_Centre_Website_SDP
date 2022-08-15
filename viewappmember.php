5c09dfa3720671eff34eb75131e01c20cb33a42a
BlueShark8J <estherlesliebala@gmail.com>
2022-08-15 13:57:33 +0800

    added more stuff for owners

---
+++ viewappmember.php
@@ -0,0 +1,63 @@
+<?php 
+
+include("conn.php");
+$result = mysqli_query($con, "SELECT * FROM centre_pages WHERE user_ID = 13") //replace 13 with user ID from session variable when logging in);
+?>
+
+<html>
+<body>
+	<title>View applications as Adoption Centre Owner/title>
+	<h2>Adoption centre page applications</h2>
+	<br>
+
+	<table>
+		<tr>
+			<td>Adoption Centre Name</td>
+			<td>SSM</td>
+			<td>Address</td>
+			<td>Phone Number</td>
+			<td>Email</td>
+			<td>Description</td>?
+			<td>Verified</td>
+		</tr>
+		<?php
+		while($row = mysqli_fetch_array($result)) {
+			echo "<tr>";
+			echo "<td>";
+			echo $row['centre_name'];
+			echo "</td>";
+
+			echo "<td>";
+			echo $row['ssm'];
+			echo "</td>";
+
+			echo "<td>";
+			echo $row['location'];
+			echo "</td>";
+
+			echo "<td>";
+			echo $row['phone'];
+			echo "</td>";
+
+			echo "<td>";
+			echo $row['email'];
+			echo "</td>";
+			
+			echo "<td>";
+			echo $row['description'];
+			echo "</td>";
+			
+			echo "<td>";
+			if ($row['verified'] == 1) {
+				echo "Verified";
+			}
+			else {
+				echo "Pending verification";
+			}
+			echo "</td></tr>";
+		}
+		mysqli_close($con);
+		?>
+	</table>
+</body>
+</html>
