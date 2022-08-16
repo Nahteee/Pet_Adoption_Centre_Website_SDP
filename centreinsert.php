8399e0b5e4a1513a792e3977a61033276dd1758e
BlueShark8J <estherlesliebala@gmail.com>
2022-08-16 21:40:56 +0800

    changed verified to have default value of 0

--- centreinsert.php
+++ centreinsert.php
@@ -5,11 +5,11 @@ include("conn.php");
 
 $image = $_FILES['centrePic']['name'];
 
-$sql="INSERT INTO centre_pages (user_ID, centre_name, ssm, location, phone, email, description, centre_pic)
+$sql="INSERT INTO centre_pages (user_ID, centre_name, ssm, location, phone, email, description, verified, centre_pic)
 
 VALUES
 
-('$_POST[userID]', '$_POST[centreName]', '$_POST[centreSSM]', '$_POST[address]', '$_POST[centrePhone]', '$_POST[centreEmail]', '$_POST[centreDesc]', '$filename')";
+('$_POST[userID]', '$_POST[centreName]', '$_POST[centreSSM]', '$_POST[address]', '$_POST[centrePhone]', '$_POST[centreEmail]', '$_POST[centreDesc]', 0, '$filename')";
 
 if (!mysqli_query($con, $sql)) {
     die("Error: " . mysqli_error($con));
