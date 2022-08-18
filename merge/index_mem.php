a981bb454cbbbc803a6526e9ca8fb8a808ec5765
Peter-Dash <110229504+Peter-Dash@users.noreply.github.com>
2022-08-18 18:27:45 +0800

    Feedback function done

--- merge/index_mem.php
+++ merge/index_mem.php
@@ -10,11 +10,14 @@ if (!isset($_SESSION['username']))  //do not allow non members to access this pa
 <?php
 // Echo session variables that were set on previous page
 echo "username is  " . $_SESSION["username"] . ".<br>";
-echo "first name is " . $_SESSION["firstname"] . ".<br>"
+echo "first name is " . $_SESSION["firstname"] . ".<br>";
+echo "role is " . $_SESSION["userRole"] . ".<br>"
 ?>
 
 <a href="./profile.php">Profile</a>
 <a href="index.php">Forum</a>
+<a href=".php">Send a Bug Report</a>
+<a href="feedback.php">give us FeedBack</a>
 
 <form action="logout.php" method="POST">
     <input type="hidden" name="destroySession" value="1">
