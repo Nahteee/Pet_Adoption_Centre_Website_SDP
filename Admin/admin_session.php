4d5c0bcdad47167d8124bfcf8c91d95ea4bd2a37
BlueShark8J <estherlesliebala@gmail.com>
2022-08-29 20:21:56 +0800

    cleared

--- Admin/admin_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['mySession']))
-{
-  echo "<script>alert('Please login!'); window.location.href='index.php';</script>";
-}
-$adminid = $_SESSION['admin_id'];
-?>
