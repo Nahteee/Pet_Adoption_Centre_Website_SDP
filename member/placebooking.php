f36267301c9a26b654b8027467c684aa926e0e37
Ethan Thong Ken Zen <ethantkz2522@gmail.com>
2022-09-02 22:10:42 +0800

    placebooking design done

--- member/placebooking.php
+++ member/placebooking.php
@@ -1,50 +1,52 @@
 <?php
-
 include("../conn.php");
+include("../header.php");
 include("../session.php");
-
 $petId = intval($_GET['id']);
-
 $pets = mysqli_query($con, "SELECT * FROM pets WHERE ID=$petId");
 ?>
-<html>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 <head>
-	<link rel="stylesheet" href = "../style.css">
+	<link rel="stylesheet" href = "../CSS/adoption.css?v=<?php echo time(); ?>">
 	<title> Adopt Pet </title>
 </head>
-<div class = "center">
-
-<?php
-if (!isset($_SESSION['userID'])) {
-	echo "pls login first";
-}
-else {
-while($row = mysqli_fetch_array($pets)) {
-
-		echo "<img src = '../uploads/" . $row['image_name'] . "' style = 'width: 300px; height: auto;'>";
-		echo "<h1>" . $row['name'] . "</h1>";
-
-		echo $row['name'] . " is " . $row['age'] . " years old<br>";
-		echo $row['name'] . " is a " . $row['species'] . "<br>";
-		echo $row['name'] . "'s breed is " . $row['breed'] . "<br>";
-		echo "<h3><br>Will you adopt " . $row['name'] . "?</h3><br>";
+<body>
 
 
-?>
+	<div class = "page-wrap">
+		<div class="outer-box">
 
-Adoption remarks: <br>
-<form action = "sendbooking.php" method = "post">
-	<input type = "hidden" name = "bookingID" value = "<?php echo $row['ID']; ?>">
-	<input type = "hidden" name = "userID" value = "<?php echo $_SESSION['userID']; ?>">
-	<input type = "hidden" name = "species" value = "<?php echo $row['species']; ?>">
-	<input type = "hidden" name = "centreID" value = "<?php echo $row['centre_ID']; ?>">
-	<textarea type="text" name="remarks"></textarea>
-	<input type = "submit" value="Adopt <?php echo $row['name']; ?>">
-</form>
-</div>
+	<?php
+	$row = mysqli_fetch_array($pets);
+			echo "<img src = '../uploads/" . $row['image_name'] . "' style = 'width: 300px; height: auto; border-radius: 15px;'>";
+			echo "<h1>" . $row['name'] . "</h1>";
 
-<?php
-	}
-}
-mysqli_close($con);
-?>
+			echo "<p>". $row['name'] . " is " . $row['age'] . " years old </p>";
+			echo "<p>". $row['name'] . " is a " . $row['species'] . "</p>";
+			echo "<p>". $row['name'] . "'s breed is " . $row['breed'] . "</p>";
+			echo "<h3><br>Will you adopt " . $row['name'] . "?</h3><br>";
+	?>
+
+	<p class='uhh'>Adoption remarks:</p>
+	<form action = "sendbooking.php" method = "post">
+		<input type = "hidden" name = "bookingID" value = "<?php echo $row['ID']; ?>">
+		<input type = "hidden" name = "userID" value = "<?php echo $_SESSION['userID']; ?>">
+		<input type = "hidden" name = "species" value = "<?php echo $row['species']; ?>">
+		<input type = "hidden" name = "centreID" value = "<?php echo $row['centre_ID']; ?>">
+		<textarea type="text" name="remarks"></textarea> <br>
+		<input type = "submit" value="Adopt <?php echo $row['name']; ?>">
+	</form>
+	</div>
+	</div>
+
+	<?php
+	mysqli_close($con);
+	?>
+
+	<footer>
+	<?php include("../footer.php") ?>
+	</footer>
+</body>
+</html>
