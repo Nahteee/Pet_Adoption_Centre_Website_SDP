6a680af6a09efbd76521c69fd3591a8b893dc82a
Ethan Thong Ken Zen <ethantkz2522@gmail.com>
2022-09-03 01:05:21 +0800

    New errors

--- owner/approvepetapp.php
+++ owner/approvepetapp.php
@@ -7,6 +7,6 @@ $id = $userid;
 mysqli_query($con,"UPDATE adoption_request SET status = 1 WHERE ID = $id");
 
 mysqli_close($con); //close database connection
-header('Location: viewapplication.php');
+header('Location: viewpetapp.php');
 
 ?>
