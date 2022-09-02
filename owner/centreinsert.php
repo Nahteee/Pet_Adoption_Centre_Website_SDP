906f3b8a994f164eb36f429402981f48b9efdbe3
Ethan Thong Ken Zen <ethantkz2522@gmail.com>
2022-09-03 01:14:02 +0800

    try centreinsert

--- owner/centreinsert.php
+++ owner/centreinsert.php
@@ -20,11 +20,12 @@ VALUES
 }
 
 else {
-    $sql="INSERT INTO centre_pages (user_ID, centre_name, ssm, location, phone, email, description, verified)
+    $file_name="default.jpg";
+    $sql="INSERT INTO centre_pages (user_ID, centre_name, ssm, location, phone, email, description, verified, centre_pic)
 
 VALUES
 
-('$id', '$_POST[centreName]', '$_POST[centreSSM]', '$_POST[address]', '$_POST[centrePhone]', '$_POST[centreEmail]', '$_POST[centreDesc]', 0)";
+('$id', '$_POST[centreName]', '$_POST[centreSSM]', '$_POST[address]', '$_POST[centrePhone]', '$_POST[centreEmail]', '$_POST[centreDesc]', 0, '$file_name')";
 }
 
 if (!mysqli_query($con, $sql)) {
